bc6fc768da75b0c1539d79a8240645bf0c410e9f
mandalmrinmoy <112158728+mandalmrinmoy@users.noreply.github.com>
2024-06-02 23:45:23 +0530

    Add files via upload

---
+++ 01_practice.py
@@ -0,0 +1,25 @@
+# sum two numbers
+def sum_two_num(num1,num2):
+    return num1+num2
+
+result=sum_two_num(2,3)
+print(result)
+
+# square a number
+def square_num(num):
+    return(num**2)
+
+result=square_num(5)
+print(result)
+
+# multiple two numbers,strings
+def multiple(a,b):
+    return(a*b)
+
+print(multiple(2,6))
+print(multiple(6,'a'))
+print(multiple(7,'a'))
+
+
+
+
